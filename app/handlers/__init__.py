99d20e49ec29a89eda5b97702bfeaf2c9cf51aa6
Azdev <jaloliddin6003@gmail.com>
2025-07-11 22:58:15 +0500

    Revert "works with docs"
    This reverts commit 54255b27dfb8420eb49cdedb5950429f8504e08f.

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,6 +1,5 @@
 from aiogram import Dispatcher
 from aiogram.filters import CommandStart, Command
-from aiogram import F
 
 from app.handlers import help, message, callback, start, commands
 
@@ -11,12 +10,7 @@ def register_handlers(dp: Dispatcher):
     dp.message(Command('admin'))(commands.admin_command)
     # messages
 
-    # message files
-    dp.message(F.document)(message.document_converter)
-
     # callbacks
     dp.callback_query(lambda c: c.data == "admin_users")(callback.admin_all_user_count)
-    # callback files
-    dp.callback_query(lambda s: s.data.startswith("docs"))(callback.docs_con)
 
     
--- app/handlers/commands.py
+++ app/handlers/commands.py
@@ -5,7 +5,7 @@ from app.config import Admin_Id
 
 #admin paneli kamandasi
 async def admin_command(message: types.Message):
-    if int(message.from_user.id) == int(Admin_Id):
+    if message.from_user.id == Admin_Id:
         await message.answer("Admin panel",reply_markup=admin_kb())
     else:
         await message.answer("Bot admini va dasturchisi: @Nufada")
